c74d6623b8a6a68a18ea82d2222b97769b99829a
prayeo <prxyeo@gmail.com>
2018-08-11 21:46:28 +0900

    Add fakedata.json & code to app.js

--- app.js
+++ app.js
@@ -1,52 +1,82 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var Graphql = require('graphql');
-// var fakeDatabase = require('./fakedata.js')
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var Graphql = require("graphql");
+var fakeDatabase = require('./fakedata.json')
+
+const geoType = new Graphql.GraphQLObjectType({ // Object 타입을 정의합니다.
+  name: "geo",
+  fields: {
+    lat: { type: Graphql.GraphQLFloat },
+    lng: { type: Graphql.GraphQLFloat }
+  }
+});
+const companyType = new Graphql.GraphQLObjectType({
+  name: "company",
+  fields: {
+    name: { type: Graphql.GraphQLString },
+    catchPhrase: { type: Graphql.GraphQLString },
+    bs: { type: Graphql.GraphQLString }
+  }
+});
+const addressType = new Graphql.GraphQLObjectType({
+  name: "address",
+  fields: {
+    street: { type: Graphql.GraphQLString },
+    suite: { type: Graphql.GraphQLString },
+    city: { type: Graphql.GraphQLString },
+    zipcode: { type: Graphql.GraphQLString },
+    geo: { type: geoType } // Object 형태일 경우 새로 GrapgQLObject를 만들어서 지정해 줍니다.
+  }
+});
+const userType = new Graphql.GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: Graphql.GraphQLInt },
+    name: { type: Graphql.GraphQLString },
+    username: { type: Graphql.GraphQLString },
+    email: { type: Graphql.GraphQLString },
+    address: { type: addressType }, // Object 형태일 경우 새로 GrapgQLObject를 만들어서 지정해 줍니다.
+    phone: { type: Graphql.GraphQLString },
+    website: { type: Graphql.GraphQLString },
+    company: { type: companyType } // Object 형태일 경우 새로 GrapgQLObject를 만들어서 지정해 줍니다.
+  }
+});
+
+var queryType = new Graphql.GraphQLObjectType({
+  name: "Query",
+  fields: {
+    user: {
+      type: userType,
+      args: { // 쿼리문 중 인자를 정의하는 곳
+        id: { type: Graphql.GraphQLInt }
+      },
+      resolve: function(id) {// 실제로 쿼리 될때 호출되는 메소드
+        const data = Object.keys(fakeDatabase).filter(element => {
+          if (fakeDatabase[element].id == id) {
+            return element;
+          }
+        });
+        return fakeDatabase[data];
+      }
+    },
+    allUser: {
+      type: new Graphql.GraphQLList(userType),
+      resolve: function(_,_,_,_) {
+        return fakeDatabase;
+      }
+    }
+  }
+});
+
+var schema = new Graphql.GraphQLSchema({ query: queryType });
+
 var app = express();
-app.use('/graphql', graphqlHTTP({
-graphiql: true,
-}));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  })
+);
 app.listen(4000);
-console.log('Running a GraphQL server');
-
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
+console.log("Running a GraphQL server");
